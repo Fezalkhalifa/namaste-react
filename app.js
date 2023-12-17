63f95030100ae30b7034030ade4f9a1a72d08fb0
Fezalkhalifa <63090868+Fezalkhalifa@users.noreply.github.com>
2023-12-17 13:59:41 -0500

    episode-01

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * <div id="Parent">
+ *       <div id="child">
+ *             <h1> i am h1 tag</h1>
+ *        </div>
+ * </div>
+ */
+
+const appendedChild = React.createElement(
+  "Div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello world from react h1 child"),
+    React.createElement("h2", {}, "Hello world from react h2 child"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello world from react h1 child2"),
+    React.createElement("h2", {}, "Hello world from react h2 child2"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "Heading" },
+  "Hello world from react"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(appendedChild);
